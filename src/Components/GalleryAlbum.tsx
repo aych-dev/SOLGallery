bcc0770454d2474ca392a6274637eb81832f290c
aych <aychdev1@gmail.com>
2023-09-09 14:23:56 -0400

    Updated noImage to show for certain collections

--- src/Components/GalleryAlbum.tsx
+++ src/Components/GalleryAlbum.tsx
@@ -1,7 +1,6 @@
 import { NftImage, nftCollections } from '../Hooks/useCollection';
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import noImage from '../Images/noImage.png';
-import React, { useState } from 'react';
 
 interface Props {
   imageData: NftImage[];
@@ -11,14 +10,17 @@ interface Props {
 const GalleryAlbum = ({ imageData, nftCollection }: Props) => {
   const albumElement = nftCollection.map((data, index) => {
     const imageIncluded = imageData.some(
-      (obj) => obj.collection === data.collection
+      (obj) =>
+        obj.collection === data.collection &&
+        data.image !== undefined &&
+        data.image !== 'none'
     );
 
     return (
       <Card key={data.id}>
         <CardMedia
           component='img'
-          alt={`Album ${data.id}`}
+          alt={`Album ${index}`}
           height='148'
           image={imageIncluded ? data.image : noImage}
         />
